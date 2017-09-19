06aaa408ae8139c93abb69948ef0b681c0f73724
Peter Dulworth <pdulworth97@gmail.com>
2017-09-19 18:49:11 -0500

    renamed

--- additional_websites/nickandcynthiaswedding/index.php
+++ additional_websites/nickandcynthiaswedding/index.php
@@ -1,3 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "peter";
+$password = "Lamborghini97";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE DATABASE myDBPDO";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Database created successfully<br>";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+?>
+
 <!DOCTYPE html>
 <html lang="eng">
   <head>

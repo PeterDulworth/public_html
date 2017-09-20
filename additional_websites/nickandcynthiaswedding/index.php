a9228bdffa4c89ac2f70a479c38faea6b602e42b
Peter Dulworth <pdulworth97@gmail.com>
2017-09-19 21:19:51 -0500

    db

--- additional_websites/nickandcynthiaswedding/index.php
+++ additional_websites/nickandcynthiaswedding/index.php
@@ -4,7 +4,7 @@ $username = "peter2";
 $password = "franz";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=nickAndCynthiaDB", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "CREATE DATABASE myDBPDO";

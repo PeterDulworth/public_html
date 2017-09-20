1819681cdd84c0b49e2eccb5105952acdb97edfb
Peter Dulworth <pdulworth97@gmail.com>
2017-09-19 21:40:02 -0500

    db

--- additional_websites/nickandcynthiaswedding/index.php
+++ additional_websites/nickandcynthiaswedding/index.php
@@ -2,15 +2,15 @@
 	$servername = "localhost";
 	$username = "peter";
 	$password = "Lamborghini97";
+	$dbname = "nickAndCynthiaDB";
+
 
 	// Create connection
-	$conn = mysqli_connect($servername, $username, $password);
-
+	$conn = new mysqli($servername, $username, $password, $dbname);
 	// Check connection
-	if (!$conn) {
-	    die("Connection failed: " . mysqli_connect_error());
-	}
-	echo "Connected successfully";
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
 
 	// sql to create table
 	$sql = "CREATE TABLE MyGuests (

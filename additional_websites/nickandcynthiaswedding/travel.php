a3f82619b5f7d9f1f911cdcddaff7d1c7d1d10bb
Peter Dulworth <pdulworth97@gmail.com>
2017-11-20 02:32:58 -0600

    more pages

---
+++ additional_websites/nickandcynthiaswedding/travel.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="eng">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Nick and Cynthia's Wedding</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet"> -->
+    <link rel="stylesheet" href="stylesheet.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row justify-content-center align-items-center">
+      	<div class="col col-12"><p class="text-center header">CYNTHIA</p></div>
+      	<div class="col col-12"><p class="text-center header-divider">and</p></div>
+      	<div class="col col-12"><p class="text-center header">NICK</p></div>
+
+      	<nav class="navbar navbar-expand-lg navbar-light" style="background-color:white;">
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		    <div class="navbar-nav">
+		      <a class="nav-item nav-link active" href="#">HOME <span class="sr-only">(current)</span></a>
+		      <a class="nav-item nav-link" href="#">EVENTS</a>
+		      <a class="nav-item nav-link" href="#">RSVP</a>
+		      <a class="nav-item nav-link" href="#">TRAVEL</a>
+		      <a class="nav-item nav-link" href="#">PHOTOS</a>
+		      <a class="nav-item nav-link" href="#">GIFT REGISTRY</a>
+		    </div>
+		  </div>
+		</nav>
+
+      	<!-- alternate image source: https://s3.amazonaws.com/uploads.hitchedup.com/e8456da1-6535-4b7c-bf71-d92b061a74bb.jpg -->
+        <div class="col col-12 mx-auto"><img src="imgs/coverphoto.jpg" class="img-fluid cover-photo" alt="Responsive image"></div>
+
+		<div class="col col-12"><h4 class="text-center">Please join us for our wedding celebration</h4></div><br><br><br>
+		<div class="col col-12"><h2 class="text-center">APRIL 28, 2018</h2></div>
+
+		<div class="col col-2 mx-auto"><img src="imgs/ampersand.png" class="mx-auto d-block"></div><br><br>
+		<br><br><br><br><br>
+		<div class="col col-12"><h3 class="text-center">Our Story</h3></div>
+
+      </div> <!-- row -->
+    </div> <!-- container -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  </body>
+</html>
